46a1053b05b86a9ccb33874a2252e5f62782740c
hdnguyendev <nguyenhd.dev@gmail.com>
2022-10-02 12:09:17 +0700

    update

--- NumberOfStep/src/Main.java
+++ NumberOfStep/src/Main.java
@@ -1,12 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        System.out.print(solution.numberOfSteps(14));
     }
 
-    class Solution {
+    static class Solution {
         public int numberOfSteps(int num) {
             int steps = 0;
-            return 0;
+
+            while (num > 0) {
+                if (num % 2 == 0) {
+                    num /= 2;
+                    steps++;
+                } else {
+                    num -= 1;
+                    steps++;
+                }
+            }
+            return steps;
         }
     }
 }
